c4c3eec14763bfc5cf77cc732f3a6adbcde47ab5
sebastian2000pk <juochoas@unal.edu.co>
2024-08-28 13:18:43 -0500

    feat: finish day button

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,28 @@ import { VITE_API_URL } from "./config";
 
 function App() {
   const [concepts, setConcepts] = useState<string>("");
+  const [sessionId, setSessionId] = useState<string>("");
+  const [studyPlanId, setStudyPlanId] = useState<string>("");
+
+  const completeSession = async () => {
+    const token = localStorage.getItem("token");
+    await fetch(`${VITE_API_URL}/tasks/today`, {
+    // await fetch(`http://localhost:3000/api/sessions/today`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        sessionId,
+        studyPlanId,
+      }),
+    });
+  };
 
   const getTasks = async () => {
     const token = localStorage.getItem("token");
-    const response = await fetch(`${VITE_API_URL}/tasks/today`, {
+    // const response = await fetch(`${VITE_API_URL}/tasks/today`, {
+    const response = await fetch(`http://localhost:3000/api/sessions/today`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -19,6 +37,8 @@ function App() {
     const data = await response.json();
     console.log("🥑 ~ getTasks ~ data:", data);
     setConcepts(data.concepts);
+    setSessionId(data.sessionId);
+    setStudyPlanId(data.studyPlanId);
   };
   useEffect(() => {
     getTasks();
@@ -32,6 +52,18 @@ function App() {
         >
           {concepts}
         </ReactMarkdown>
+
+        {sessionId && (
+          <div
+            style={{
+              padding: "1rem 2rem",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={completeSession}>Complete leasson</button>
+          </div>
+        )}
       </div>
     </>
   );
